158ebbeec9b2c3ec46fe2ad16b1b359bc0e8b9a1
劉俊廷 <M10407209@mail.ntust.edu.tw>
2015-08-20 23:44:37 +0800

    test

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on 2015年8月20日, 下午 11:02
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("EGG");
+    printf("opps");
+
+    return (EXIT_SUCCESS);
+}
+
